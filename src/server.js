7dfa143770ccf9cfcc0c76e5343acf883eb7e291
othiagomanhaes <othiagomanhaes@gmail.com>
2023-01-31 20:21:43 -0300

    Adicionando o nodemon para ganho de produtividade.

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 // src/server.js
 const app = require('./app');
 
-app.listen(3001, () => console.log('server running on port 3001'));
+app.listen(3001, () => console.log('Servidor ONLINE na porta 3001’'));
