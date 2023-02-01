d667e26ee72671463d045915305aa368edbbbfef
othiagomanhaes <othiagomanhaes@gmail.com>
2023-01-31 23:46:38 -0300

    Editando times por id e buscando por id.

--- src/app.js
+++ src/app.js
@@ -22,11 +22,33 @@ app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
 
 app.get('/teams', (req, res) => res.status(200).json({ teams }));
 
+app.get('/teams/:id', (req, res) => {
+    const { id } = req.params;
+    const chosenTeam = teams.find((team) => team.id === Number(id));
+
+    res.status(200).json({ chosenTeam });
+});
+
 app.post('/teams', (req, res) => {
     const newTeam = { ...req.body };
     teams.push(newTeam);
 
     res.status(201).json({ team: newTeam });
+});
+
+app.put('/teams/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, initials } = req.body;
+
+    const updateTeam = teams.find((team) => team.id === Number(id));
+
+    if (!updateTeam) {
+        res.status(404).json({ message: 'time não encontrado' });
+    }
+
+    updateTeam.name = name;
+    updateTeam.initials = initials;
+    res.status(200).json({ updateTeam });
 });
 
 module.exports = app;
